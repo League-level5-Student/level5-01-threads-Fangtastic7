7d07f454c0e4457f1d0c8306b59c569514d5df23
Fangtastic7 <mwfang7@gmail.com>
2020-07-03 18:54:16 -0700

    ThreadedGreeter, ThreadPoolTest

--- src/_03_Threaded_Greeting/ThreadedGreeter.java
+++ src/_03_Threaded_Greeting/ThreadedGreeter.java
@@ -1,11 +1,26 @@
 package _03_Threaded_Greeting;
 
 public class ThreadedGreeter implements Runnable {
+	private int i;
+	public ThreadedGreeter(int i) {
+		// TODO Auto-generated constructor stub
+		this.i = i;
+	//	System.out.println(i);
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+if(i<=50) {
+	Thread x = new Thread(new ThreadedGreeter(i+1));
+	x.start();
+	System.out.println("Hello from thread number: " + i);
+	try {
+		x.join();
+	} catch(InterruptedException e) {
+		e.printStackTrace();
+	}
+}
 	}
 
 }
--- src/_04_Thread_Pool/ThreadPool.java
+++ src/_04_Thread_Pool/ThreadPool.java
@@ -1,5 +1,39 @@
 package _04_Thread_Pool;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class ThreadPool {
+	private Thread[] array;
+	private ConcurrentLinkedQueue<Task> taskQueue;
+	public ThreadPool(int totalthreads) {
+		// TODO Auto-generated constructor stub
+		array = new Thread[totalthreads];
+		taskQueue = new ConcurrentLinkedQueue<Task>();
+		for(int i=0;i<array.length;i++) {
+			array[i] = new Thread(new Worker(taskQueue));
+		}
+			
+	}
+
+	
+
+	public void start() {
+		// TODO Auto-generated method stub
+		for(int i=0;i<array.length;i++) {
+			array[i].start();
+			try {
+				array[i].join();
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+
+
+	public void addTask(Task task) {
+		// TODO Auto-generated method stub
+		taskQueue.add(task);
+	}
 
 }
